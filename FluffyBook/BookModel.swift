6fee6ac52cf2b51d8ff0a0f4ad8126b1237c2947
wuzzapcom <wuzzapcom@gmail.com>
2017-03-13 23:29:33 +0300

    Added pictures to TabBar. Updated loading text from BookModel to Controller

--- FluffyBook/BookModel.swift
+++ FluffyBook/BookModel.swift
@@ -13,13 +13,13 @@ class BookModel{
     
     func getCurrentProgressPercent() -> Float {
         
-        return 0.0
+        return 50.0
         
     }
     
     func getTextFromCurrentPage() -> String {
         
-        return "Hello! this is page 1 text"
+        return "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
         
     }
     
