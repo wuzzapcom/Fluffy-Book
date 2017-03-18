48089ca827142c35324773325305ad06c602d84b
Biogen <tsukanovaa@student.bmstu.ru>
2017-03-18 17:18:33 +0300

    Added CocoaPods lib

---
+++ FluffyBook/BookParserModel.swift
@@ -0,0 +1,21 @@
+//
+//  BookParserModel.swift
+//  FluffyBook
+//
+//  Created by Alexandr Tsukanov on 18.03.17.
+//  Copyright © 2017 wuzzapcom. All rights reserved.
+//
+
+import Foundation
+import FolioReaderKit
+
+class BookParserModel {
+    
+    func openEpub() {
+        
+    }
+    
+    func parseEpub() -> Int32 {
+        return 0
+    }
+}
